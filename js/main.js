89f592a0ba5a99cfb6dd75d3e3444377e31d69da
Alireza Baghban <alireza.baghbann@gmail.com>
2024-12-04 16:17:41 +0100

    navbar - footer - search bar added

--- js/main.js
+++ js/main.js
@@ -122,3 +122,48 @@ fetch(url, options)
   .catch((error) => {
     console.error("Error fetching popular movies:", error);
   });
+
+// -------------------------------------------------
+// To the Top
+$(document).ready(function () {
+  // Create the Back-to-Top button and its styles dynamically
+  var back_to_top_button = [
+    '<a href="#top" class="back-to-top" style="',
+    "position: fixed; ",
+    "bottom: 8px; ",
+    "right: 1px; ",
+    "padding: 1em; ",
+    "z-index: 100; ",
+    "color: white; ",
+    "text-align: center; ",
+    "border-radius: 5px; ",
+    "font-family: Arial, sans-serif; ",
+    "font-size: 50px; ",
+    "text-decoration: none; ",
+    "display: none; ",
+    '">⇧</a>',
+  ].join("");
+
+  // Append the button to the body
+  $("body").append(back_to_top_button);
+
+  // Handle the scroll event to toggle visibility
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".back-to-top").fadeIn().addClass("flash-effect");
+    } else {
+      $(".back-to-top").fadeOut().removeClass("flash-effect");
+    }
+  });
+
+  // Smooth scroll to the top when the button is clicked
+  $(".back-to-top").click(function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      800
+    );
+    return false;
+  });
+});
