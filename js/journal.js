5be09a976cb8401ef7fcc5c7dc7e40adeebe5744
ilonavich <ilonavich@yahoo.de>
2024-12-05 15:11:21 +0100

    wrote some more javascript based on discussion

--- js/journal.js
+++ js/journal.js
@@ -9,10 +9,25 @@ function getFavorites() {
   return JSON.parse(localStorage.getItem("favorites")); //retrieve entry name favorites from local storage localStorage.getItem(favorites);
 }
 
-//function to handle movie note updates
-function updateMovieNote(movieID, notetext) {
+function getFavoriteById(favorites, id) {
+  return favorites.find((movie) => movie !== null && movie.id === id); //find looks for first entry in an array of favorites that corresponds to a certain criteria
+}
+
+function removeFavoriteById(id) {
+  //look at entries of favorites and search the first entry that corresponds my criteria and return index or posision in an array
+  //criteria is movie.id === id
   const favorites = getFavorites(); // Aufrufen einer Funktion
-  const movie = favorites[movieID]; //first parameter in the object favorites, retrieve object named in movieID from favorites
+  const index = favorites.findIndex(
+    (movie) => movie !== null && movie.id === id
+  );
+  favorites.splice(index, 1);
+  updateFavorites(favorites);
+}
+
+//function to handle movie note updates
+function updateMovieNoteById(id, notetext) {
+  const favorites = getFavorites(); // Aufrufen einer Funktion
+  const movie = getFavoriteById(favorites, id); //first parameter in the object favorites, retrieve object named in movieID from favorites
   movie.notetext = notetext; //set a property named notetext
   updateFavorites(favorites); //call function updateFavorites with value named favorites
 }
